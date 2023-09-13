7af4346981d4dd1404712308c0dd5c54c5924273
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-13 15:31:37 -0400

    Add all stock app endpoints

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # path(),
     path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/stock", include("easy_trade.stock.urls")),
 ]
 
 if settings.DEBUG:
