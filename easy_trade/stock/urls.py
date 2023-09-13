09c1c05000ce052aaa8cbca37f820cbd6653bcd9
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-13 16:38:38 -0400

    Add endpoint for list historical stock prices

--- easy_trade/stock/urls.py
+++ easy_trade/stock/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from easy_trade.stock.views import StockView
+from easy_trade.stock.views import StockHistoricalView, StockView
 
 urlpatterns = [
     path("", StockView.as_view()),
+    path("/historical", StockHistoricalView.as_view()),
 ]
