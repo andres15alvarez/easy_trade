97a075ce94ac2453a6226bd62b1036fa19b661ae
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-13 16:33:28 -0400

    Create serializer for historical stock list

--- easy_trade/stock/serializers.py
+++ easy_trade/stock/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from easy_trade.stock.models import Stock
+from easy_trade.stock.models import HistoricalStock, Stock
 
 
 class StockSerializer(ModelSerializer):
     class Meta:
         model = Stock
         fields = "__all__"
+
+
+class HistoricalStockSerializer(ModelSerializer):
+    class Meta:
+        model = HistoricalStock
+        exclude = ["stock"]
