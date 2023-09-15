07425e0e63b01e4a780371f493bb9f28374fe943
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-15 18:59:42 -0400

    Create serializer for account

---
+++ easy_trade/account/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from easy_trade.account.models import Account, AccountTransaction, Card
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ["balance"]
+
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        exclude = ["user"]
+        extra_kwargs = {"cvv": {"write_only": True}}
+
+
+class AccountTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountTransaction
+        exclude = ["account"]
