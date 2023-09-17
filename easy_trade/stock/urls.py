7c071d8e8e5f6954f0e3241224a47c16383fee46
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-17 16:06:06 -0400

    Add endpoint for wallets

--- easy_trade/stock/urls.py
+++ easy_trade/stock/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from easy_trade.stock.views import StockHistoricalView, StockView
+from easy_trade.stock.views import StockHistoricalView, StockView, WalletView
 
 urlpatterns = [
     path("", StockView.as_view()),
     path("/historical", StockHistoricalView.as_view()),
+    path("/wallet", WalletView.as_view()),
 ]
