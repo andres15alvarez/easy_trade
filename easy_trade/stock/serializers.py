29e5439b99db5729c26b6fceec12610d72ef9881
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-17 16:05:01 -0400

    Create serializer for walltet

--- easy_trade/stock/serializers.py
+++ easy_trade/stock/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework.serializers import ModelSerializer
 
-from easy_trade.stock.models import HistoricalStock, Stock
+from easy_trade.stock.models import HistoricalStock, Stock, Wallet
 
 
 class StockSerializer(ModelSerializer):
     class Meta:
         model = Stock
         fields = "__all__"
+
+
+class StockWalletSerializer(ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ["name", "code", "current_price"]
+
+
+class WalletSerializer(ModelSerializer):
+    stock = StockWalletSerializer()
+
+    class Meta:
+        model = Wallet
+        exclude = ["user"]
 
 
 class HistoricalStockSerializer(ModelSerializer):
