f81db5ad0c399da7ddbe56ba519ae9c873949585
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-17 16:05:36 -0400

    Create view to list the wallter of the user

--- easy_trade/stock/views.py
+++ easy_trade/stock/views.py
@@ -1,12 +1,23 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from easy_trade.stock.models import HistoricalStock, Stock
-from easy_trade.stock.serializers import HistoricalStockSerializer, StockSerializer
+from easy_trade.stock.models import HistoricalStock, Stock, Wallet
+from easy_trade.stock.serializers import (
+    HistoricalStockSerializer,
+    StockSerializer,
+    WalletSerializer,
+)
+
+
+class WalletView(generics.RetrieveAPIView):
+    def get(self, request) -> Response:
+        queryset = Wallet.objects.filter(user=request.user)
+        serializer = WalletSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class StockView(generics.ListAPIView):
-    def list(self, *args, **kwargs):
+    def list(self, request):
         queryset = Stock.objects.filter()
         serializer = StockSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -21,7 +32,7 @@ class StockHistoricalView(generics.ListAPIView):
             day__range=(date_from, date_to), stock__code=code
         ).select_related("stock")
 
-    def list(self, *args, **kwargs):
+    def list(self, request):
         queryset = self.get_queryset()
         serializer = HistoricalStockSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
