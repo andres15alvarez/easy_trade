4478f97bafac6cf4650a9b7501d6a3fb7248c99d
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-17 11:38:51 -0400

    Create method to deposit or withdraw funds

--- easy_trade/account/views.py
+++ easy_trade/account/views.py
@@ -35,3 +35,26 @@ class AccountTransactionView(APIView):
         queryset = AccountTransaction.objects.filter(account__user=request.user)
         serializer = AccountTransactionSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = AccountTransactionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        account = Account.objects.get(user=request.user)
+        if serializer.validated_data["card"].user != request.user:
+            return Response(
+                {"error": "Tarjeta invalida"}, status=status.HTTP_406_NOT_ACCEPTABLE
+            )
+        if (
+            serializer.validated_data["amount"] > account.balance
+            and serializer.validated_data["type"] == "withdraw"
+        ):
+            return Response(
+                {"error": "Saldo insuficiente"}, status=status.HTTP_406_NOT_ACCEPTABLE
+            )
+        transaction = serializer.save(account=account)
+        if transaction.type == "deposit":
+            account.balance += transaction.amount
+        else:
+            account.balance -= transaction.amount
+        account.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
