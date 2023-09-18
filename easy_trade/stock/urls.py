1f74a5c58993a3977e2943c6b341739d66117d70
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-18 07:52:26 -0400

    Register endpoint for stock transaction

--- easy_trade/stock/urls.py
+++ easy_trade/stock/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from easy_trade.stock.views import StockHistoricalView, StockView, WalletView
+from easy_trade.stock.views import (
+    StockHistoricalView,
+    StockTransactionView,
+    StockView,
+    WalletView,
+)
 
 urlpatterns = [
     path("", StockView.as_view()),
     path("/historical", StockHistoricalView.as_view()),
     path("/wallet", WalletView.as_view()),
+    path("/transaction", StockTransactionView.as_view()),
 ]
