05d31d3641cc179d8287c451cdc7b132137594a3
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-18 07:46:00 -0400

    Create serializer for stock transaction

--- easy_trade/stock/serializers.py
+++ easy_trade/stock/serializers.py
@@ -1,21 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from easy_trade.stock.models import HistoricalStock, Stock, Wallet
+from easy_trade.stock.models import HistoricalStock, Stock, Transaction, Wallet
 
 
-class StockSerializer(ModelSerializer):
+class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = "__all__"
 
 
-class StockWalletSerializer(ModelSerializer):
+class StockWalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ["name", "code", "current_price"]
 
 
-class WalletSerializer(ModelSerializer):
+class WalletSerializer(serializers.ModelSerializer):
     stock = StockWalletSerializer()
 
     class Meta:
@@ -23,7 +23,23 @@ class WalletSerializer(ModelSerializer):
         exclude = ["user"]
 
 
-class HistoricalStockSerializer(ModelSerializer):
+class HistoricalStockSerializer(serializers.ModelSerializer):
     class Meta:
         model = HistoricalStock
         exclude = ["stock"]
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    stock = StockWalletSerializer()
+
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+
+
+class CreateTransactionSerializer(serializers.ModelSerializer):
+    type = serializers.ChoiceField(choices=["buy", "sale"])
+
+    class Meta:
+        model = Transaction
+        fields = ["stock", "quantity"]
