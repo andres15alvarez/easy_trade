75341697f9930e5d0606bc44efe9566cee57bf26
Andres Alvarez <andres15alvarez@gmail.com>
2023-09-18 19:38:36 -0400

    Create update password view

--- easy_trade/user/serializers.py
+++ easy_trade/user/serializers.py
@@ -13,3 +13,15 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data.get("password"))
         return super().create(validated_data)
+
+
+class UserPasswordSerializer(serializers.Serializer):
+    old = serializers.CharField(max_length=128)
+    new = serializers.CharField(max_length=128)
+
+    def validate(self, attrs):
+        if attrs["old"] == attrs["new"]:
+            raise serializers.ValidationError(
+                "Las nueva contraseña debe ser distinta a la anterior"
+            )
+        return attrs
--- easy_trade/user/views.py
+++ easy_trade/user/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password, make_password
 from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
@@ -5,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from easy_trade.account.models import Account
-from easy_trade.user.serializers import UserSerializer
+from easy_trade.user.serializers import UserPasswordSerializer, UserSerializer
 
 
 class UserView(APIView):
@@ -22,3 +23,15 @@ class UserView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        serializer = UserPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if not check_password(serializer.validated_data["old"], request.user.password):
+            return Response(
+                {"error": "Contraseña antigua incorrecta"},
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
+        request.user.password = make_password(serializer.validated_data["new"])
+        request.user.save()
+        return Response(status=status.HTTP_200_OK)
